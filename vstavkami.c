13f529cd7f0b1ca9a51104de83cc4ef58edfb819
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-11-12 23:20:46 +0300

    Update vstavkami.c

--- vstavkami.c
+++ vstavkami.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-using namespace std;
-
 int main()
 {
-	int i, j, b;
-	int const k = 6;
+
+	int i, j, x;
+	const int k = 6;
 	int a[k];
 
 	printf("Vvedite massiv\n");
@@ -14,14 +13,14 @@ int main()
 		scanf_s("%d", &a[i]);
 	}
 
-	for (i = 1; i < k; i++)
+	for (i = 0; i < k; i++)
 	{
-		for (j = i; j > 0 && a[j] < a[j - 1]; j--)
+		x = a[i];
+		for (j = i; j > 0 && x < a[j - 1]; j--)
 		{
-			b = a[j - 1];
-			a[j - 1] = a[j];
-			a[j] = b;
+			a[j] = a[j - 1];
 		}
+		a[j] = x;
 	}
 
 	for (i = 0; i < k; i++)
